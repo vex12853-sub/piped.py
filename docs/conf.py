c3279ca82eb6f37d986686b6dba9a37f8a417e7f
vex12853 <admin@vex12853.net>
2024-03-10 11:33:16 +0900

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python3
+"""pytube documentation build configuration file."""
+import os
+import sys
+
 import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath("../"))
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -16,7 +23,15 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+]
+
+autosummary_generate = True
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -27,4 +42,5 @@ language = 'ja'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
